9f806eb4f56ef9320c24484102838d8651095f81
Hasan Jamilov <hasanjamilov01@gmail.com>
2023-09-18 16:36:15 +0400

    added second task

--- function_task.py
+++ function_task.py
@@ -18,3 +18,31 @@ for i in range(n):
 print("Арифметическая прогрессия:")
 for element in arithmetic_progression:
     print(element)
+
+# --------------------------------------------------------------------------------------------------------------
+
+# Задача 32: Определить индексы элементов массива (списка), значения которых принадлежат заданному диапазону (т.е. не меньше заданного минимума и не больше заданного максимума)
+
+# Функция для поиска индексов элементов в заданном диапазоне
+def find_indices_in_range(arr, min_value, max_value):
+    indices = []
+    for i in range(len(arr)):
+        if min_value <= arr[i] <= max_value:
+            indices.append(i)
+    return indices
+
+# Пример списка
+my_list = [10, 5, 15, 8, 20, 25, 3]
+
+# Заданный диапазон
+min_value = 5
+max_value = 15
+
+# Находим индексы элементов в заданном диапазоне
+result_indices = find_indices_in_range(my_list, min_value, max_value)
+
+# Выводим результат на экран
+if result_indices:
+    print(f"Индексы элементов в диапазоне от {min_value} до {max_value}: {result_indices}")
+else:
+    print("В заданном диапазоне нет элементов.")
